b76ef074bf00153b7d07cdc5c72caab1332368ce
lgalan <lein.galan@sine95.com>
2025-09-27 17:52:04 -0400

    Dev1: Renombra las funciones a 'sumar' y 'restar'

--- routers/operaciones.py
+++ routers/operaciones.py
@@ -1,4 +1,4 @@
-def sumar_numeros(a, b):
+def sumar(a, b):
     """Esta función suma dos números y devuelve el resultado."""
     return a + b
 
@@ -7,10 +7,10 @@ num1 = 1
 num2 = 2
 
 # Llamar a la función y mostrar el resultado
-resultado = sumar_numeros(num1, num2)
+resultado = sumar(num1, num2)
 print(f"La suma de {num1} y {num2} es {resultado}")
 
-def restar_numeros(a, b):
+def restar(a, b):
     """
     Esta funcion toma dos números (a y b) y devuelve el resultado de a - b.
     """
@@ -23,7 +23,7 @@ numero1 = 10
 numero2 = 4
 
 # Llamar a la función
-resultado = restar_numeros(numero1, numero2)
+resultado = restar(numero1, numero2)
 
 # Imprimir el resultado
 print(f"La resta de {numero1} menos {numero2} es: {resultado}")
