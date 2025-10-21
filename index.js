0a4866c8c57102aa188d853bed8bd38ca5af598b
lgalan <lein.galan@sine95.com>
2025-10-21 19:12:34 -0400

    Implementacion inicial de la funcion sumar

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+/**
+ * Funcion simple para sumar dos numeros.
+ * Creada para practicar la integracion de Git con VS Code.
+ */
+function sumar(a, b) {
+    return a + b;
+}
+
+const resultado = sumar(15, 7);
+console.log("El resultado de la suma es:", resultado);
