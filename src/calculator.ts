cc3e1e9a2d7c2ef684e4ac8bb94dc8dc3e8318ae
lgalan <lein.galan@sine95.com>
2025-10-23 18:00:43 -0400

    feat: Anadir comentario de version 2.0 a la funcion add

--- src/calculator.ts
+++ src/calculator.ts
@@ -6,5 +6,7 @@
  * @returns La suma de a y b.
  */
 export function add(a: number, b: number): number {
+   // TU CAMBIO: Añades un comentario de versión
+  // Version 2.0: Suma simple
   return a + b;
 }
